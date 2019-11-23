1afb5376eaef0160419ece2c37c149ab4bd180e5
Panat <panat.taranat@gmail.com>
2019-11-23 17:32:53 -0500

    Add PokemonCenter

---
+++ PokemonCenter.cpp
@@ -0,0 +1,95 @@
+#include "PokemonCenter.h"
+
+// Default constructor initializes the member variables to these values:
+// - display_code = 'C'
+// - stamina_capacity = 100
+// - num_stamina_points = stamina_capacity
+// - dollar_cost_per_stamina_point = 5
+PokemonCenter::PokemonCenter() {
+  display_code = 'C';
+  state = STAMINA_POINTS_AVAILABLE;
+  std::cout << "PokemonCenter default constructed.\n";
+}
+// Initializes the member variables to these values:
+// - ID number = in_id
+// - location = in_loc
+// - dollar_cost_per_stamina_point = stamina_cost
+// - stamina_capacity = stamina_cap
+// The rest are default.
+PokemonCenter::PokemonCenter(int in_id, double stamina_cost,
+                             unsigned int stamina_cap, Point2D in_loc) {
+  id_num = in_id;
+  location = in_loc;
+  dollar_cost_per_stamina_point = stamina_cost;
+  stamina_capacity = stamina_cap;
+  display_code = 'C';
+  state = STAMINA_POINTS_AVAILABLE;
+  std::cout << "PokemonCenter constructed.\n";
+}
+// Returns true if this PokemonCenter contains at least one stamina point.
+// Returns false otherwise.
+bool PokemonCenter::HasStaminaPoints() {
+  if (GetNumStaminaPointsRemaining() >= 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Returns the number of stamina points remaining in this Pokemon Center.
+unsigned int PokemonCenter::GetNumStaminaPointsRemaining() {
+  return num_stamina_points_remaining;
+}
+// Returns true if this Pokemon can afford to purchase stamina_points with the
+// given budget.
+bool PokemonCenter::CanAffordStaminaPoints(unsigned int stamina_points,
+                                           double budget) {
+  if (GetDollarCost(stamina_points) - budget >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Returns the Pokemon dollar cost for the specified number of stamina points.
+double PokemonCenter::GetDollarCost(unsigned int stamina_points) {
+  return stamina_points * dollar_cost_per_stamina_point;
+}
+// if the amount num_stamina_points_remaining in the Pokemon Center is greater
+// than or equal to points_needed, it subtracts points_needed from
+// PokemonCenter’s amount and returns points_needed. If the amount of stamina
+// points in the PokemonCenter is less, it returns the PokemonCenter’s current
+// amount, and the PokemonCenter stamina amount is set to 0.
+unsigned int PokemonCenter::DistributeStamina(unsigned int points_needed) {
+  if (GetNumStaminaPointsRemaining() >= points_needed) {
+    num_stamina_points_remaining -= points_needed;
+    return points_needed;
+  } else {
+    unsigned int temp = num_stamina_points_remaining;
+    num_stamina_points_remaining = 0;
+    return temp;
+  }
+}
+// If the PokemonCenter has no stamina points remaining
+// - Its state is set to NO_STAMINA_POINTS_AVAILABLE
+// - display_code is changed to ‘c’
+// - Prints PokemonCenter (id) has ran out of stamina points.
+// - Returns true if stamina is depleted; returns false if it is not depleted.
+bool PokemonCenter::Update() {
+  if (GetNumStaminaPointsRemaining() == 0 &&
+      state != NO_STAMINA_POINTS_AVAILABLE) {
+    state = NO_STAMINA_POINTS_AVAILABLE;
+    display_code = 'c';
+    std::cout << "PokemonCenter " << id_num
+              << " has ran out of stamina points.\n";
+    return true;
+  } else {
+    return false;
+  }
+}
+// Prints out status of the object
+void PokemonCenter::ShowStatus() {
+  std::cout << "Pokemon Center Status: ";
+  Building::ShowStatus();
+  std::cout << "Pokemon dollars per stamina point: "
+            << dollar_cost_per_stamina_point << "\nhas "
+            << num_stamina_points_remaining << " stamina point(s) remaining.\n";
+}
---
+++ PokemonCenter.h
@@ -0,0 +1,62 @@
+#ifndef POKEMONCENTER_H
+#define POKEMONCENTER_H
+
+#include "Building.h"
+
+class PokemonCenter : public Building {
+  enum PokemonCenterStates {
+    STAMINA_POINTS_AVAILABLE = 0,
+    NO_STAMINA_POINTS_AVAILABLE = 1
+  };
+
+ private:
+  // The maximum number of stamina points this Pokemon Center can hold.
+  unsigned int stamina_capacity = 100;
+  // The amount of stamina currently in this Pokemon Center, initially
+  // stamina_capacity
+  unsigned int num_stamina_points_remaining = stamina_capacity;
+  // The per stamina point cost in Pokemon Dollars
+  double dollar_cost_per_stamina_point = 5;
+
+ public:
+  // Default constructor initializes the member variables to these values:
+  // - display_code = 'C'
+  // - stamina_capacity = 100
+  // - num_stamina_points = stamina_capacity
+  // - dollar_cost_per_stamina_point = 5
+  PokemonCenter();
+  // Initializes the member variables to these values:
+  // - ID number = in_id
+  // - location = in_loc
+  // - dollar_cost_per_stamina_point = stamina_cost
+  // - stamina_capacity = stamina_cap
+  // The rest are default.
+  PokemonCenter(int in_id, double stamina_cost, unsigned int stamina_cap,
+                Point2D in_loc);
+  // Returns true if this PokemonCenter contains at least one stamina point.
+  // Returns false otherwise.
+  bool HasStaminaPoints();
+  // Returns the number of stamina points remaining in this Pokemon Center.
+  unsigned int GetNumStaminaPointsRemaining();
+  // Returns true if this Pokemon can afford to purchase stamina_points with the
+  // given budget.
+  bool CanAffordStaminaPoints(unsigned int stamina_points, double budget);
+  // Returns the Pokemon dollar cost for the specified number of stamina points.
+  double GetDollarCost(unsigned int stamina_points);
+  // if the amount num_stamina_points_remaining in the Pokemon Center is greater
+  // than or equal to points_needed, it subtracts points_needed from
+  // PokemonCenter’s amount and returns points_needed. If the amount of stamina
+  // points in the PokemonCenter is less, it returns the PokemonCenter’s current
+  // amount, and the PokemonCenter stamina amount is set to 0.
+  unsigned int DistributeStamina(unsigned int points_needed);
+  // If the PokemonCenter has no stamina points remaining
+  // - Its state is set to NO_STAMINA_POINTS_AVAILABLE
+  // - display_code is changed to ‘c’
+  // - Prints PokemonCenter (id) has ran out of stamina points.
+  // - Returns true if stamina is depleted; returns false if it is not depleted.
+  bool Update();
+  // Prints out status of the object
+  void ShowStatus();
+};
+
+#endif
