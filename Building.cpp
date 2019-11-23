a158280be89e14666187a196e00ae05781410ff6
Panat <panat.taranat@gmail.com>
2019-11-23 16:49:28 -0500

    Add comments

--- Building.cpp
+++ Building.cpp
@@ -1,8 +1,12 @@
 #include "Building.h"
 
+// Default constructor, display_code is 'B'
 Building::Building() : GameObject('B') {
   std::cout << "Building default constructed.\n";
 }
+// Initializes the id number to in_id, and the location to in_loc, display_code
+// to in_code, and remainder of the member variables to their default initial
+// values.
 Building::Building(char in_code, int in_id, Point2D in_loc)
     : GameObject(in_code) {
   id_num = in_id;
@@ -12,6 +16,7 @@ Building::Building(char in_code, int in_id, Point2D in_loc)
 }
 void Building::AddOnePokemon() { pokemon_count += 1; }
 void Building::RemoveOnePokemon() { pokemon_count -= 1; }
+// Prints “(pokemon_count) pokemon is/are in this building”
 void Building::ShowStatus() {
   if (pokemon_count == 1) {
     std::cout << pokemon_count << " Pokemon is in this building\n";
@@ -19,4 +24,5 @@ void Building::ShowStatus() {
     std::cout << pokemon_count << " Pokemon are in this building\n";
   }
 }
+// Returns true because buildings are always visible
 bool Building::ShouldBeVisible() { return true; }
