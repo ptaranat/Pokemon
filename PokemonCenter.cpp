a7ac83947788e2b0f8df561e26855ca091f6260b
Panat <panat.taranat@gmail.com>
2019-11-23 18:37:17 -0500

    Update PokemonCenter.cpp
    Use GetNumStam function instead

--- PokemonCenter.cpp
+++ PokemonCenter.cpp
@@ -91,6 +91,7 @@ void PokemonCenter::ShowStatus() {
   std::cout << "Pokemon Center Status: ";
   Building::ShowStatus();
   std::cout << "Pokemon dollars per stamina point: "
-            << dollar_cost_per_stamina_point << "\nhas "
-            << num_stamina_points_remaining << " stamina point(s) remaining.\n";
+            << dollar_cost_per_stamina_point << '\n'
+            << "has " << GetNumStaminaPointsRemaining()
+            << " stamina point(s) remaining.\n";
 }
