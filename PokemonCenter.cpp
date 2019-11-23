9e8557c7ccfc709150250786233a6fa767dbccc3
Panat <panat.taranat@gmail.com>
2019-11-23 18:50:46 -0500

    Revert "Update PokemonCenter.cpp"
    This reverts commit af290562636ab88a2f495d51c76d47bc83eed43c.

--- PokemonCenter.cpp
+++ PokemonCenter.cpp
@@ -91,7 +91,7 @@ void PokemonCenter::ShowStatus() {
   std::cout << "Pokemon Center Status: ";
   Building::ShowStatus();
   std::cout << "Pokemon dollars per stamina point: "
-            << GetDollarCost(1) << '\n'
+            << dollar_cost_per_stamina_point << '\n'
             << "has " << GetNumStaminaPointsRemaining()
             << " stamina point(s) remaining.\n";
 }
