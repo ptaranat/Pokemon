799d0203b6fe4e02f942a16b21113f401768d57b
Panat <panat.taranat@gmail.com>
2019-11-23 19:45:38 -0500

    Overload == operator
    Needed for Pokemon compare destination and current location

--- Point2D.cpp
+++ Point2D.cpp
@@ -25,3 +25,10 @@ Point2D operator+(const Point2D p1, const Vector2D v1) {
 Vector2D operator-(const Point2D p1, const Point2D p2) {
   return Vector2D(p1.x - p2.x, p1.y - p2.y);
 }
+bool operator==(const Point2D p1, const Point2D p2) {
+  if (p1.x == p2.x && p1.y == p2.y) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- Point2D.h
+++ Point2D.h
@@ -16,4 +16,5 @@ double GetDistanceBetween(const Point2D p1, const Point2D p2);
 std::ostream& operator<<(std::ostream& os, const Point2D p1);
 Point2D operator+(const Point2D p1, const Vector2D v1);
 Vector2D operator-(const Point2D p1, const Point2D p2);
+bool operator==(const Point2D p1, const Point2D p2);
 #endif
